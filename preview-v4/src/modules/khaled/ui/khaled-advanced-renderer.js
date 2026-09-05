84c43c423211c288dbb1774c9c01f977df314913
modynawe-code <modynawe@gmail.com>
2026-09-05 19:30:09 +0300

    Register chapter eleven renderer

--- preview-v4/src/modules/khaled/ui/khaled-advanced-renderer.js
+++ preview-v4/src/modules/khaled/ui/khaled-advanced-renderer.js
@@ -1,8 +1,10 @@
 import { isMeasurementQuestion, renderMeasurementQuestion } from './khaled-measurement-renderer.js';
+import { isNumberPatternsQuestion, renderNumberPatternsQuestion } from './khaled-number-patterns-renderer.js';
 
-export function isAdvancedQuestion(question){return isMeasurementQuestion(question);}
+export function isAdvancedQuestion(question){return isMeasurementQuestion(question)||isNumberPatternsQuestion(question);}
 
 export function renderAdvancedQuestion(args){
   if(isMeasurementQuestion(args.question))return renderMeasurementQuestion(args);
+  if(isNumberPatternsQuestion(args.question))return renderNumberPatternsQuestion(args);
   return false;
 }
