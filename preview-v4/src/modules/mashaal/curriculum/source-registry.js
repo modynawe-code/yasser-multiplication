fb0dc36a7247d3cd8308a5be400b063abe587433
modynawe-code <modynawe@gmail.com>
2026-09-08 07:54:43 +0300

    Mashaal: record authoritative KG3 source provenance

---
+++ preview-v4/src/modules/mashaal/curriculum/source-registry.js
@@ -0,0 +1,8 @@
+export const MASHAAL_SOURCE_REGISTRY = Object.freeze({
+  'saudi-curriculum-guide-2025':Object.freeze({
+    authority:'Saudi Ministry of Education / National Curriculum Center',
+    title:'Curriculum Guide - Fifth Edition',
+    year:2025,
+    supports:Object.freeze(['kg3-age-range','kg3-six-learning-domains'])
+  })
+});
