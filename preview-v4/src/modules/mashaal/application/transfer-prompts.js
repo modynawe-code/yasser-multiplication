45cadbf9f248aa0613b58a1d48e4a7ad2a3af436
modynawe-code <modynawe@gmail.com>
2026-09-08 07:53:25 +0300

    Mashaal: add initial off-screen transfer prompts

---
+++ preview-v4/src/modules/mashaal/application/transfer-prompts.js
@@ -0,0 +1,6 @@
+export const MASHAAL_TRANSFER_PROMPTS = Object.freeze({
+  'count-and-quantity':Object.freeze(['عدّي أربع ألعاب من حولك.']),
+  'shapes-space':Object.freeze(['دوري حولك على شيء دائري.']),
+  'classify-sort':Object.freeze(['اجمعي ثلاثة أشياء ورتبيها حسب اللون.']),
+  'healthy-habits':Object.freeze(['وريني خطوات غسل اليدين.'])
+});
