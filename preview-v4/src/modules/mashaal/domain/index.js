1b8ca329371796caa3cbae34d9ef6bcd536e4a41
modynawe-code <modynawe@gmail.com>
2026-09-08 08:02:15 +0300

    Mashaal: expose KG3 domain model

---
+++ preview-v4/src/modules/mashaal/domain/index.js
@@ -0,0 +1,3 @@
+export { createInitialMashaalState, normalizeMashaalState } from './state-model.js';
+export { createMashaalSkillProgress, normalizeMashaalSkillProgress } from './progress-model.js';
+export { MASHAAL_DATA_SCHEMA_VERSION, MASHAAL_STORAGE_NAMESPACE } from './constants.js';
