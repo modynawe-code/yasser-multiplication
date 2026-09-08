1f20047df5b999151229624b0594994e8b76debd
modynawe-code <modynawe@gmail.com>
2026-09-08 08:04:35 +0300

    Architecture: expose open curriculum API

---
+++ preview-v4/src/shared/curricula/index.js
@@ -0,0 +1,5 @@
+export { CURRICULUM_REGISTRY, getCurriculum, listCurricula } from './curriculum-registry.js';
+export { normalizeCurriculumId } from './curriculum-id.js';
+export { createCurriculumProfile } from './curriculum-profile.js';
+export { createCurriculumRegistry } from './runtime-registry.js';
+export { CURRICULUM_REGISTRY_CONTRACT } from './registry-contract.js';
