b76dfaf403be2101d4e31c6fa05ea32e116ead97
modynawe-code <modynawe@gmail.com>
2026-09-08 11:04:48 +0300

    Add approved KG3 recitation source policy

---
+++ preview-v4/src/modules/mashaal/curriculum/recitation-source-registry.js
@@ -0,0 +1,39 @@
+const KFGQPC_AUDIO_RIGHTS_URL='https://qc-dev.qurancomplex.gov.sa/quran-audios/';
+
+export const MASHAAL_RECITATION_SOURCES=Object.freeze({
+  'kfgqpc-ibrahim-al-akhdar-hafs':Object.freeze({
+    id:'kfgqpc-ibrahim-al-akhdar-hafs',
+    authority:'King Fahd Glorious Quran Printing Complex',
+    authorityAr:'مجمع الملك فهد لطباعة المصحف الشريف',
+    reciter:'Ibrahim Al-Akhdar',
+    reciterAr:'إبراهيم الأخضر',
+    riwayah:'Hafs from Asim',
+    riwayahAr:'حفص عن عاصم',
+    humanVoice:true,
+    sourceApproved:true,
+    rightsStatus:'explicit-public-use-for-applications',
+    rightsUrl:KFGQPC_AUDIO_RIGHTS_URL,
+    sourcePage:'https://qurancomplex.gov.sa/quran-audio-hafs-akhdar/',
+    sourcePackage:'https://download.qurancomplex.gov.sa/new-sounds/akhdar/hafs/akhdar-sura.zip',
+    childLearningMode:Object.freeze(['listen','repeat','replay']),
+    syntheticRecitationAllowed:false,
+    localMediaReady:false,
+    localAssets:Object.freeze([])
+  })
+});
+
+export const MASHAAL_DEFAULT_RECITATION_SOURCE_ID='kfgqpc-ibrahim-al-akhdar-hafs';
+
+export function getMashaalRecitationSource(id=MASHAAL_DEFAULT_RECITATION_SOURCE_ID){
+  return MASHAAL_RECITATION_SOURCES[id]||null;
+}
+
+export function getMashaalRecitationMediaStatus(id=MASHAAL_DEFAULT_RECITATION_SOURCE_ID){
+  const source=getMashaalRecitationSource(id);
+  return Object.freeze({
+    sourceId:source?.id||null,
+    sourceApproved:Boolean(source?.sourceApproved&&source?.humanVoice&&source?.rightsStatus==='explicit-public-use-for-applications'),
+    localMediaReady:Boolean(source?.localMediaReady&&source?.localAssets?.length),
+    syntheticRecitationAllowed:Boolean(source?.syntheticRecitationAllowed)
+  });
+}
