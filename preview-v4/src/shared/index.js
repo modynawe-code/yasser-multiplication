2f318027c38723d1e9fcd6f9f850b033232b09d2
modynawe-code <modynawe@gmail.com>
2026-09-14 19:36:12 +0300

    feat: expose shared question bank

--- preview-v4/src/shared/index.js
+++ preview-v4/src/shared/index.js
@@ -3,3 +3,4 @@ export * as Curricula from './curricula/index.js';
 export * as Activities from './activities/index.js';
 export * as Progress from './progress/index.js';
 export * as Family from './family/index.js';
+export * as QuestionBank from './question-bank/index.js';
